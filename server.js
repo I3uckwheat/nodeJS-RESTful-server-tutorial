31d7973a7452c51fd57dea40a384eb90075f1bbf
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-08 20:21:07 -0600

    adds middleware

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const logger = require('morgan');
 const errorhandler = require('errorhandler');
 const bodyParser = require('body-parser');
+
+let store = {};
+store.accounts = [];
+
+let app = express();
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(errorhandler());
