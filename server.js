3b6cf52abdea3f24bc34f13c9528c6e78183c3ad
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-08 20:36:37 -0600

    completes resful API example

--- server.js
+++ server.js
@@ -10,3 +10,26 @@ let app = express();
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(errorhandler());
+
+app.get('/accounts', (req, res) => {
+  res.status(200).send(store.accounts)
+});
+
+app.post('/accounts', (req, res) => {
+  let newAccount = req.body
+  let id = store.accounts.length
+  store.accounts.push(newAccount)
+  res.status(201).send({id: id})
+})
+
+app.put('/accounts/:id', (req, res) => {
+  store.accounts[req.params.id] = req.body
+  res.status(200).send(store.accounts[req.params.id])
+})
+
+app.delete('/accounts/:id', (req, res) => {
+  store.accounts.splice(req.params.id, 1)
+  res.status(204).send()
+})
+
+app.listen(3000);
